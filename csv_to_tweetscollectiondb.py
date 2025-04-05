8677ccc8bfd703a743f9720da3aa84ffa8686eb6
PiyushRathi1056 <piyushrathi105@gmail.com>
2025-04-06 01:50:28 +0530

    Update csv_to_tweetscollectiondb.py

--- csv_to_tweetscollectiondb.py
+++ csv_to_tweetscollectiondb.py
@@ -1,17 +1,13 @@
 import csv
 import pymongo
 
-# Replace this with your actual connection string
-client = pymongo.MongoClient("mongodb+srv://piyushrathi105:19QXv6uNNlARGBiy@cluster0.grxmynq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client=pymongo.MongoClient("mongodb+srv://piyushrathi105:19QXv6uNNlARGBiy@cluster0.grxmynq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db=client["HappyMaps"]
+collection=db["tweets"]
 
-# Connect to the database and collection
-db = client["HappyMaps"]
-collection = db["tweets"]  # You can change this to whatever you want
-
-# Read CSV and insert into MongoDB
 with open("data.csv", newline='', encoding='utf-8') as csvfile:
-    reader = csv.DictReader(csvfile)
-    data = list(reader)
+    reader=csv.DictReader(csvfile)
+    data=list(reader)
     
     '''for i, row in enumerate(reader, start=1):
         row["index"] = i  # Add custom index starting from 1
@@ -20,6 +16,6 @@ with open("data.csv", newline='', encoding='utf-8') as csvfile:
     if data:
         collection.delete_many({})  # This clears all existing data in the collection
         collection.insert_many(data)
-        print("CSV data inserted successfully!")
+        print("Data was successfully added in the tweets collection of the Database")
     else:
         print("No data found in CSV.")
